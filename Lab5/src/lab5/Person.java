a6cd1412a81cde393dca0ccc6f393b76c25d180d
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-03-26 07:47:53 +0200

    compulsory

---
+++ Lab5/src/lab5/Person.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ */
+package lab5;
+
+/**
+ *
+ * @author rebec
+ */
+public record Person(int id, String name) {
+}
